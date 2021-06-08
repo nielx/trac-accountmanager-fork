102b78a5c1b092b4a9b4ed9cba5804e6d4515b74
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2021-06-08 09:32:35 +0100

    Update algorithm to work on Python 3 bytes type

--- acct_mgr/md5crypt.py
+++ acct_mgr/md5crypt.py
@@ -10,7 +10,7 @@
 import hashlib
 
 
-def md5crypt(password, salt, magic='$1$'):
+def md5crypt(password: bytes, salt: bytes, magic: bytes = b'$1$') ->bytes:
     # /* The password first, since that is what is most unknown */ /*
     # Then our magic string */ /* Then the raw salt */
     m = hashlib.md5()
@@ -19,16 +19,16 @@ def md5crypt(password, salt, magic='$1$'):
     # /* Then just as many characters of the MD5(pw,salt,pw) */
     mixin = hashlib.md5(password + salt + password).digest()
     for i in range(0, len(password)):
-        m.update(mixin[i % 16])
+        m.update(bytes([mixin[i % 16]]))
 
     # /* Then something really weird... */
     # Also really broken, as far as I can tell.  -m
     i = len(password)
     while i:
         if i & 1:
-            m.update('\x00')
+            m.update(bytes([0]))
         else:
-            m.update(password[0])
+            m.update(bytes([password[0]]))
         i >>= 1
 
     final = m.digest()
@@ -56,41 +56,41 @@ def md5crypt(password, salt, magic='$1$'):
 
     # This is the bit that uses to64() in the original code.
 
-    itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    itoa64 = b'./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
-    rearranged = ''
+    rearranged = bytearray()
     for a, b, c in (
             (0, 6, 12), (1, 7, 13), (2, 8, 14), (3, 9, 15), (4, 10, 5)):
-        v = ord(final[a]) << 16 | ord(final[b]) << 8 | ord(final[c])
+        v = final[a] << 16 | final[b] << 8 | final[c]
         for i in range(4):
-            rearranged += itoa64[v & 0x3f]
+            rearranged.append(itoa64[v & 0x3f])
             v >>= 6
 
-    v = ord(final[11])
+    v = final[11]
     for i in range(2):
-        rearranged += itoa64[v & 0x3f]
+        rearranged.append(itoa64[v & 0x3f])
         v >>= 6
 
-    return magic + salt + '$' + rearranged
+    return magic + salt + b'$' + rearranged
 
 
 if __name__ == '__main__':
 
     def test(clear_password, the_hash):
-        magic, salt = the_hash[1:].split('$')[:2]
-        magic = '$' + magic + '$'
+        magic, salt = the_hash[1:].split(b'$')[:2]
+        magic = b'$' + magic + b'$'
         return md5crypt(clear_password, salt, magic) == the_hash
 
 
     test_cases = (
-        (' ', '$1$yiiZbNIH$YiCsHZjcTkYd31wkgW8JF.'),
-        ('pass', '$1$YeNsbWdH$wvOF8JdqsoiLix754LTW90'),
-        ('____fifteen____', '$1$s9lUWACI$Kk1jtIVVdmT01p0z3b/hw1'),
-        ('____sixteen_____', '$1$dL3xbVZI$kkgqhCanLdxODGq14g/tW1'),
-        ('____seventeen____', '$1$NaH5na7J$j7y8Iss0hcRbu3kzoJs5V.'),
-        ('__________thirty-three___________',
-         '$1$HO7Q6vzJ$yGwp2wbL5D7eOVzOmxpsy.'),
-        ('apache', '$apr1$J.w5a/..$IW9y6DR0oO/ADuhlMF5/X1')
+        (b' ', b'$1$yiiZbNIH$YiCsHZjcTkYd31wkgW8JF.'),
+        (b'pass', b'$1$YeNsbWdH$wvOF8JdqsoiLix754LTW90'),
+        (b'____fifteen____', b'$1$s9lUWACI$Kk1jtIVVdmT01p0z3b/hw1'),
+        (b'____sixteen_____', b'$1$dL3xbVZI$kkgqhCanLdxODGq14g/tW1'),
+        (b'____seventeen____', b'$1$NaH5na7J$j7y8Iss0hcRbu3kzoJs5V.'),
+        (b'__________thirty-three___________',
+         b'$1$HO7Q6vzJ$yGwp2wbL5D7eOVzOmxpsy.'),
+        (b'apache', b'$apr1$J.w5a/..$IW9y6DR0oO/ADuhlMF5/X1')
     )
 
     for clearpw, hashpw in test_cases:
